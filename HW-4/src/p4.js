764bd7cb20674d9ac35d6fa019895913b4cad665
Parham Alvani <parham.alvani@gmail.com>
2015-11-26 17:15:32 +0330

    Problem 4 was added.

--- HW-4/src/p4.js
+++ HW-4/src/p4.js
@@ -3,17 +3,15 @@ var results = db.links.find({
 })
 
 function negative_comment(comment, index, comments){
-		if (comment.upVotes == 0 || comment.downVotes >= 3) {
+		if (comment.upVotes == 0 && comment.downVotes >= 3) {
 			printjson(comment);
-		} else if (min == null) {
-			min = comment;
-		} else if (max != null && max.upVotes - max.downVotes < comment.upVotes - comment.downVotes) {
-			max = comment;
-		} else if (max == null) {
-			max = comment;
+		} else if (comment.upVotes <= 20 && (comment.downVotes / comment.upVotes) > 4) {
+			printjson(comment);
+		} else if (comment.upVotes > 20 && (comment.downVotes / comment.upVotes) > 6) {
+			printjson(comment);
 		}
 		if ('replies' in comment) {
-			comment.replies.every(best_worst_comment);
+			comment.replies.every(negative_comment);
 		}
 }
 
